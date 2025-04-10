b6b63dea961f5d39138b0806278f86a612b2efd3
Brandon2395 <bacosta30762@ufide.ac.cr>
2025-04-10 16:49:42 -0600

    Crud doctores
    Se creo la lista de doctores, se puede agregar, activar y desactivar. Falta la vista de editar y en crear que traiga la lista de sucursales.
    
    Además se modifico citas para que salga el nombre y apellido del doctor

--- src/routes/doctoresRoutes.js
+++ src/routes/doctoresRoutes.js
@@ -3,6 +3,25 @@ const router = express.Router();
 const doctorController = require('../controllers/doctoresController'); 
 
 // Ruta para obtener la lista de doctores
+router.get('/doctores/citas', doctorController.getDoctoresCitas);
+
+// Ruta para obtener la lista de doctores
 router.get('/doctores', doctorController.getDoctores);
+
+// Ruta para crear un doctor
+router.post('/doctores/crear', doctorController.crearDoctor);
+
+// Ruta para activar un doctor
+router.get('/doctores/activar/:id', doctorController.activarDoctor);
+
+// Ruta para desactivar un doctor
+router.get('/doctores/desactivar/:id', doctorController.desactivarDoctor);
+
+// Ruta para editar un doctor
+router.get('/doctores/editar/:id', doctorController.editarDoctor);
+
+// Ruta para actualizar un doctor
+router.post('/doctores/editar/:id', doctorController.actualizarDoctor);
+
 
 module.exports = router;
