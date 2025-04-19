dfe4f4be7046771908faa3e6c9e3955de4ecef8e
Bryan Perez <bperezr@outlook.com>
2025-04-19 17:07:46 -0600

    .

--- src/controllers/citasV2Controller.js
+++ src/controllers/citasV2Controller.js
@@ -6,9 +6,9 @@ class CitasController{
     async crearCita(req, res) {
         try {
             const data = req.body;
-            console.log("entrando a crear cita" && req.body);
-            const cita = await citasService.crearCita(data);            
-            return res.status(201).json(cita);
+            await citasService.crearCita(data); 
+
+            res.redirect('/admin/crearCitas?agregado=1');
           } catch (err) {
             console.error('Error al crear cita:', err);
             return res.status(400).json({ error: err.message });
